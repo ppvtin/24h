20f7529206d1b72bfd545a5b1a0b5512008441c6
ppvt <phanphungtin@gmail.com>
2020-07-03 11:40:04 +0700

    update user

--- user/about.php
+++ user/about.php
@@ -9,197 +9,26 @@
         <link rel="stylesheet" type="text/css" href="assets/vendor/owl-slider.css"/>
         <link rel="stylesheet" type="text/css" href="assets/vendor/settings.css"/>
         <link rel="shortcut icon" href="assets/images/favicon.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,500,700,300' rel='stylesheet' type='text/css'>
         <script type="text/javascript" src="assets/js/jquery-3.2.0.min.js"></script>
         <title>Thông Tin</title>
     </head>
     <body>
-    <div class="awe-page-loading">
-         <div class="awe-loading-wrapper">
-            <div class="awe-loading-icon">
-               <img src="assets/images/logo.png" alt="images">
-            </div>
-            <div class="progress">
-               <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-         </div>
-      </div>    
-    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content popup-search">
-                <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i></button>
-                <div class="modal-body">
-                    <div class="input-group">
-                        <input type="text" class="form-control control-search" placeholder="Search and hit enter...">
-                        <button class="button_search" type="button">Search</button>
-                    </div><!-- /input-group -->
-
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div>
-    <!-- End pushmenu -->
-    <!-- End pushmenu -->
-    <div class="wrappage">
-        <header id="header" class="header-v3 header-v2">
-            <div id="topbar" class="topbar-ver2">
-                <div class="container container-ver2">
-                    <div class="inner-topbar box">
-                        <div class="float-left">
-                            <p><img src="assets/images/icon-phone-header.png.png" alt="icon">Liên hệ chúng tôi <span> 070-7782-9137</span></p>
-                        </div>
-                        <div class="float-right align-right">
-                            <div class="hover-menu">
-                                <a class="acc" href="#" title="USD"><img src="assets/images/icon-user-header.png" alt="icon">YOUR ACCOUNT</a>
-                                <ul class="list-menu">
-                                    <li><a href="#" title="USD">LOGIN</a></li>
-                                    <li><a href="#" title="VND">REGISTER</a></li>
-                                </ul>
-                            </div>
-                            <!-- End hover-menu -->
-                        </div>
-                    </div>
-                </div>
-                <!-- End container -->
-            </div>
-            <!-- End topbar -->
-            <div class="header-top">
-                <div class="container container-ver2">
-                    <div class="box">
-                        <p class="icon-menu-mobile"><i class="fa fa-bars" ></i></p>
-                        <div class="logo"><a href="#" title="Uno">
-                            <img src="assets/images/logo-v2.png" alt="images">
-                        </a></div>
-                        <div class="logo-mobile"><a href="#" title="Xanadu"><img src="assets/images/logo-v2.png" alt="Xanadu-Logo"></a></div>
-
-                        <div class="box-right">
-                            
-                            <div class="search dropdown" data-toggle="modal" data-target=".bs-example-modal-lg">
-                                <i class="icon"></i>
-                            </div>
-                        </div>
-                        <nav class="mega-menu">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                          <ul class="nav navbar-nav" id="navbar">
-                            <li class="level1 active"><a href="#" title="Home">Home</a>
-                                <ul class="menu-level-1 dropdown-menu">
-                                   <!-- <li class="level2"><a href="home_v1.html" title="Home 1">Home 1</a></li>
-                                    <li class="level2"><a href="home_v2.html" title="Home 2">Home 2</a></li>
-                                    <li class="level2"><a href="home_v3.html" title="Home 3">Home 3</a></li>
-                                    <li class="level2"><a href="home_v4.html" title="Home 4">Home 4</a></li> -->
-                                </ul>
-                            </li>
-                              <li class="level1"><a href="about.html" title="About us">About us</a></li>
-                            <li class="level1 dropdown">
-                                <a href="#" title="men">Store</a>
-                                <div class="sub-menu sub-menu-v2 dropdown-menu">
-                                <div class="top-sub-menu">
-                                    <img src="assets/images/top-submenu1.jpg" alt="images">
-                                </div>
-                                  <ul class="menu-level-1">
-                                    <li class="level2"><a href="#">Living Room</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="#" title="Seatings">Seatings</a></li>
-                                            <li class="level3"><a href="#" title="Tables">Tables</a></li>
-                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
-                                            <li class="level3"><a href="#" title="Series">Series</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="level2"><a href="#">BATH Room</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="#" title="Lighting">Lighting</a></li>
-                                            <li class="level3"><a href="#" title="Storage">Storage</a></li>
-                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
-                                            <li class="level3"><a href="#" title="Beds">Beds</a></li>
-                                        </ul>
-                                    </li>
-                                     <li class="level2"><a href="#">KITCHEN Room</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="#" title="Cleaning Tools">Cleaning Tools</a></li>
-                                            <li class="level3"><a href="#" title="Waste Bins">Waste Bins</a></li>
-                                            <li class="level3"><a href="#" title="Pots & Pans">Pots & Pans</a></li>
-                                            <li class="level3"><a href="#" title="Beds">Beds</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="level2"><a href="#">Living Room</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="#" title="Seatings">Seatings</a></li>
-                                            <li class="level3"><a href="#" title="Tables">Tables</a></li>
-                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
-                                            <li class="level3"><a href="#" title="Series">Series</a></li>
-                                        </ul>
-                                    </li>
-                                  </ul>
-                                  <!-- End RightSubMenu -->
-                              </div>
-                              <!-- End Dropdow Menu -->
-                            </li>
-                            <li class="level1 dropdown"><a href="#" title="Page">Page</a>
-                                <div class="sub-menu dropdown-menu">
-                                  <ul class="menu-level-1">
-                                    <li class="level2"><a href="#">Shop Page</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="order.html" title="Order">Order</a></li>
-                                            <li class="level3"><a href="shopping_cart.html" title="Shopping Cart">Shopping Cart</a></li>
-                                            <li class="level3"><a href="checkout.html" title="Check Out">Check Out</a></li>
-                                            <li class="level3"><a href="wishlist.html" title="Wish List">Wish List</a></li>
-                                            <li class="level3"><a href="checkout.html" title="Check Out">Check Out</a></li>
-                                            <li class="level3"><a href="catalog_fullwidth.html" title="Shop Grid Full">Shop Full</a></li>
-                                            <li class="level3"><a href="catalog_sidebar.html" title="Shop Left Sidebar">Shop Left Sidebar </a></li>
-                                            <li class="level3"><a href="catalog_sidebar_right.html" title="Shop Right Sidebar">Shop Right Sidebar</a></li>
-
-                                        </ul>
-                                    </li>
-                                    <li class="level2"><a href="#">Other Page</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="FAQ.html" title="FAQ">FAQ</a></li>
-                                            <li class="level3"><a href="page_404.html" title="Page 404">Page 404</a></li>
-                                            <li class="level3"><a href="popup.html" title="Newsletter">Newsletter</a></li>
-                                            <li class="level3"><a href="quick_view.html" title="Quick View">Quick View</a></li>
-                                            <li class="level3"><a href="login_register.html" title="Login Registers">Login Register</a></li>
-                                            <li class="level3"><a href="tracking.html" title="Tracking">Tracking</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="level2"><a href="#">Blog Page</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="blog_2_columns.html" title="Blog 2 Columns">Blog 2 Columns</a></li>
-                                            <li class="level3"><a href="blog_3_columns.html" title="Blog 3 Columns">Blog 3 Columns</a></li>
-                                            <li class="level3"><a href="blog_3_columns_masonry.html" title="Blog 3 Columns Masonry">Blog 3 Columns Masonry</a></li>
-                                            <li class="level3"><a href="blog_details.html" title="Blog Details">Blog Details</a></li>
-                                            <li class="level3"><a href="blog_list_width.html" title="Blog List Width">Blog List Width</a></li>
-                                        </ul>
-                                    </li>
-                                     <li class="level2"><a href="#">Signle Page</a>
-                                        <ul class="menu-level-2">
-                                            <li class="level3"><a href="product_detail.html" title="Version 1">Product Detail Version 1</a></li>
-                                            <li class="level3"><a href="product_detail2.html" title="Version 2">Product Detail Version 2</a></li>
-                                            <li class="level3"><a href="product_detail3.html" title="Version 3">Product Detail Version 3</a></li>
-                                            <li class="level3"><a href="product_detail4.html" title="Version 4">Product Detail Version 4</a></li>
-                                        </ul>
-                                    </li>
-                                  </ul>
-                                  <!-- End RightSubMenu -->
-                              </div>
-                              <!-- End Dropdow Menu -->
-                            </li>
-                            <li class="level1"><a href="blog_details.html" title="collections">Blog</a></li>
-                            <li class="level1"><a href="contact.html" title="Contact us">Contact us</a></li>
-                          </ul>
-                        </nav>
-                    </div>
-                </div>
-                <!-- End container -->
-            </div>
-            <!-- End header-top -->
+  <body>
+        <tr>
+              <td>
+                 <?php
+                 include("menu-banner.php");
+                 ?>
+              </td>
+        </tr>
+        <!-- End Banner_Menu-->
         </header><!-- /header -->
-        <div class="container">
-            <div class="banner-header banner-lbook3 space-30">
-                <img src="assets/images/banner-blog.jpg" alt="Banner-header">
-                <div class="text">
-                    <h3>Thông tin về chúng tôi</h3>
-                </div>
-            </div>
+        <div class="container" style="margin: 50px 0px;" >
+            <h3 style="text-align: center; font-size: 120px; font-weight: bold; font-family: 'Bangers', cursive;"><a href="about.php">Thông tin về 24h Team</a></h3>
         </div>
+
         <!-- End container -->
         <div class="page-about">
             <div class="container container-ver2 space-50 head-about">
@@ -219,7 +48,7 @@
                 <!--End choose-us-->
                 <div class="row">
                     <div class="col-md-6">
-                        <img class="img-responsive" src="assets/images/about-head1.jpg" alt="images">
+                        <img class="img-responsive" src="assets/images/logovl.png" alt="images">
                     </div>
                     <!--End col-md-6-->
                     <div class="col-md-6 pd-left-10">
